0e415462384b6fd68efd292770f0763979ecf5e8
aye_work1 <amoe.aye@gmail.com>
2021-04-12 21:04:24 -0700

    added detete resource button on front-end

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,6 @@
 import { describe } from "mocha";
 
+
 describe('Testing All Routes', () => {
     it('Visit Resources.us!', () => {
         cy.visit('http://localhost:4000/')
@@ -25,21 +26,29 @@ describe('Create Resource', () => {
     it('click on Create Resource button', () =>{
         cy.contains('Create Resource').click()        
         cy.url().should('include', '/CreateResource')
-    })
+    });
 
     it('Inserting values in from', () =>{
         cy.get('input[name="title"]').type('Testing Javascript'); 
         cy.get('input[name="link"]').type('https://testingjavascript.com/'); 
         cy.get('input[name="description"]').type('Medium Article about testing with javascript');
         cy.get('input[name="category"]').type('Testing');
-        cy.get('select').select('js.j');       
+        cy.get('select').select('js.j');    
         
-    })
+    });
 
-    // it('submitting the create resource form', () => {
-    //     cy.get('form button').focus().type('{enter}');
-    //     cy.url().should('include', '/teams/606fa2b39146be28386764d3')
-    // })
+    it('submitting the create resource form', () => {
+        cy.get('form button').focus().type('{enter}');
+        cy.url().should('include', '/teams/606fa2b39146be28386764d3')
+    });
+
+    it('deleting the created resource', () => {
+        cy.get('div[class="link"]').contains('https://testingjavascript.com/');
+        cy.contains('x').click();
+    });    
+
+
+
 
     
 })
